cedab99973c1b57b4314c2b0d76f8143b88de8b2
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-12 13:20:48 +0200

    github-pages deployments

--- src/index.jsx
+++ src/index.jsx
@@ -21,7 +21,9 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+], {
+  basename: "/OPAKOVANI-PROJEKT-XXXMuck"
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
