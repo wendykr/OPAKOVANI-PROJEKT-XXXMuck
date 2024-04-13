d1a95673d66db1a4f85a5da8ad25d66ace73d062
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-13 16:17:22 +0200

    fix tag header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <div className="header">
+    <header className="header">
       <div className="header__container">
         <NavLink to="/" className="header__link"><h1 className="header__title">XXXMuck</h1></NavLink>
       </div>
-    </div>
+    </header>
   )
 }
