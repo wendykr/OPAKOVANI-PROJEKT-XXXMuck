d3be35758009c154795b1792a47b40c085e860fa
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-15 13:41:59 +0200

    move fetchProducts

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
-import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('https://apps.kodim.cz/react-2/xxxmuck/products');
-        if (!response.ok) {
-          if (response.status === 400) {
-            setError('Load data error');
-            setIsLoading(false);
-          } else {
-            setError('Load data error');
-            setIsLoading(false);
-          }
-          return;
-        }
-        const data = await response.json();
-        setProducts(data);
-        setIsLoading(false);
-      } catch (error) {
-        setError('Load data error');
-        setIsLoading(false);
-        console.error('Chyba při spojení s API:', error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
 
   return (
     <div className="container">
       <Header />
-      <Outlet products={products} isLoading={isLoading} error={error} />
+      <Outlet />
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,41 @@ import { HomePage } from './pages/HomePage/HomePage';
 import { ProductPage } from './pages/ProductPage/ProductPage';
 import { ErrorPage } from './pages/ErrorPage/ErrorPage';
 
+let products = [];
+let error = null;
+let isLoading = true;
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://apps.kodim.cz/react-2/xxxmuck/products');
+    if (!response.ok) {
+      if (response.status === 400) {
+        error = 'Load data error';
+        isLoading = false;
+      } else {
+        error = 'Load data error';
+        isLoading = false;
+      }
+      return;
+    }
+    const data = await response.json();
+    products = data;
+    console.log(products);
+    isLoading = false;
+  } catch (err) {
+    error = 'Load data error';
+    isLoading = false;
+    console.error('Chyba při spojení s API:', error);
+  }
+};
+
+fetchProducts();
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/product/:productId" element={<ProductPage />} />
+      <Route path="/" products={products} isLoading={isLoading} error={error} element={<HomePage />} />
+      <Route path="/product/:productId" products={products} isLoading={isLoading} error={error} element={<ProductPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   ),
